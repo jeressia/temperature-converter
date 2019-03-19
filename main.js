f2649271a391922edd83ea732e01ba928fde8d53
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-03-18 22:51:46 -0500

    convert button makes input show in output area

--- main.js
+++ main.js
@@ -1,47 +1,37 @@
 
-const temp = [
-    {
-    temperature:"1000deg"
-    },
-];
-
 const printToDom = (divId, textToPrint) => {
     const selectedDiv=document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
 
-const buttonEvents = () => {
-    document.getElementById('convertBtn').addEventListener('click',determineConverter);
-    document.getElementById('clearBtn').addEventListener('click',determineConverter);
- };
+ const tempInput = (document.getElementById('tempInput'));
+ 
 
- const tempInput = () => {
-    document.getElementById("tempInput").value;
- }
+//  const showTemp= () => {
+//     if (celsiusRadio.checked === true) {
+//     let domString='';
+//         domString +=    `<h1 class="answer">Jeressia</h1>`;
+//     printToDom('tempOutput',domString);
+//     }
+//     if (fahrenheitRadio.checked === true) {
+   
+//     };
+// }
 
- const showTemp= () => {
-    if (celsiusRadio.checked === true) {
-    let domString='';
-        domString +=    `<h1>tempInput(); degrees Celsius</h1>`;
-    printToDom('tempOutput',domString);
-    }
-    if (fahrenheitRadio.checked === true) {
-    let domString='';
-    domString +=    `<h1>${temp.temperature} degrees Fahrenheit</h1>`;
-printToDom('tempOutput',domString);
-    };
-}
-
-    showTemp();
+    // showTemp();
+    const buttonEvents = () => {
+        document.getElementById('convertBtn').addEventListener('click',determineConverter);
+        document.getElementById('clearBtn').addEventListener('click',determineConverter);
+     };
 
 const determineConverter = (e) => {
     const buttonId = e.target.id;
       if (buttonId === 'convertBtn' ){
-        console.log('You clicked convert');
+    printToDom('tempOutput',tempInput.value);
   };
- }
+ };
   const init = () => {
-      determineConverter();
+      buttonEvents();
   }
 
   init();
